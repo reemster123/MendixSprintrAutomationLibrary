82f76acb4091fae06ada0c04e7f6f4761f0b7121
reemster123 <42781824+reemster123@users.noreply.github.com>
2020-12-22 18:46:25 +0100

    Update navigateToEnvironmentsPage.js
    required the navigateToApp function

--- mainfunctions/navigateToEnvironmentsPage.js
+++ mainfunctions/navigateToEnvironmentsPage.js
@@ -4,23 +4,11 @@ const gf = require('./genericfunctions.js');
 
 // navigateToEnvironmentspage
 module.exports = async function(appName, page) {
-     await page.goto(gv.myappsUrl, { waitUntil: "networkidle2"});
-     console.log('Went to my apps page...');
-     await page.waitForSelector('.page-apps__card', {timeout: gv.standardTimeOutWFS});
-     console.log('searching for app with name: '+ appName+'...');
-     const appfound = await page.evaluate((name) => {
-        let appCards = document.querySelectorAll('.page-apps__card');
-        // appcards isnt an actual array, it is a DOMobject. we should create an array from it and then we can find/map/filer etc.
-        appCards = Array.from(appCards);
-        const el = appCards.find(card => card.getElementsByTagName('p')[0].innerText.toLowerCase() === name);
-        if (typeof el !== 'undefined') {
-             el.click();
-             return true;
-        }
-        return false;
-    }, appName); 
- 
+    //navigate to the apps homepage.
+    const appfound = require('./navigateToApp.js')(appName, page);
     if (appfound) { 
+        // navigate to environmentspage of app.
+        console.log('App with name "'+appName+'" exists...');
         await page.waitForSelector('#formatstring_widget_formatstring_14', {timeout: gv.standardTimeOutWFS});       
         const envElement = await page.$('#formatstring_widget_formatstring_14');
         // wait extra seconds for page to load, this page takes some time.
