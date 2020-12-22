5b5b50cc6e1bbadd6a4c6e3ccd7537c15d25f073
reemster123 <42781824+reemster123@users.noreply.github.com>
2020-12-22 19:41:59 +0100

    Update navigateToEnvironmentsPage.js
    turned the clickonAppMenuItem function into a require, sothat it can be used seperately

--- mainfunctions/navigateToEnvironmentsPage.js
+++ mainfunctions/navigateToEnvironmentsPage.js
@@ -1,31 +1,15 @@
-// import globalVariables and genericFunctions
-const gv = require('../globalvariables.json');
-const gf = require('./genericfunctions.js');
 
 // navigateToEnvironmentspage
 module.exports = async function(appName, page) {
     //navigate to the apps homepage.
     const appfound = await require('./navigateToApp.js')(appName, page);
     if (appfound) { 
-        // navigate to environmentspage of app.
         console.log('App with name "'+appName+'" exists...');
-        await page.waitForSelector('#formatstring_widget_formatstring_14', {timeout: gv.standardTimeOutWFS});       
-        const envElement = await page.$('#formatstring_widget_formatstring_14');
-        // wait extra seconds for page to load, this page takes some time.
-        await gf.delay(gv.standardDelayAfterPageLoad);
-        await clickOnEnvironments(envElement, page);
+        // go to the environments page
+        await require('./clickOnAppMenuItem')('#formatstring_widget_formatstring_14', page);
     } else {
         console.log('Appname not found...');
     }
     return appfound;    
 }
 
-async function clickOnEnvironments(envElement, page) {
-    const anchorTagHtml = await page.evaluate((env)=> { 
-        return env.children[0].innerHTML;
-    }, envElement);
-    console.log('Navigating to Environments page...');
-    let envLink = anchorTagHtml.split("\"")[1].split("\"")[0];
-    console.log('envUrl: '+envLink );
-    await page.goto(envLink, {waitUntil: 'networkidle2'} );
-}
