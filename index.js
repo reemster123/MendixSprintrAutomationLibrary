4006512a879d6f45202ca474ae7f4ec763b97383
reemster123 <42781824+reemster123@users.noreply.github.com>
2020-12-23 18:31:19 +0100

    Update index.js
    required selectDeployablePackage. originally this function was private for this file. now it is moved to its own file
    
    shutdown function is retrieved by genericfunctions (gv)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function main() {
         console.log('Opening browser...');
         const browser = await puppeteer.launch({headless: !gv.showProcesInBrowser});
         const page = await require('./mainfunctions/setuppage.js')(browser);
-
+        
         //login with credentials
         await require('./mainfunctions/login.js')(page);
         await gf.delay(gv.standardDelayAfterFunction);
@@ -30,45 +30,23 @@ async function main() {
             await gf.delay(gv.standardDelayAfterPageLoad);
 
             // Create deployment package
-            const packageUploaded =  true;//await require('./mainfunctions/uploadpackage.js')(brancheName, page);
+            const packageUploaded = await require('./mainfunctions/uploadpackage.js')(brancheName, page);
             await gf.delay(gv.standardDelayAfterPageLoad);
     
             if (packageUploaded) {
                 //Deploy package
-                await selectDeployablePackage(page); 
+                await require('./mainfunctions/selectDeployablePackage.js')(page); 
                 await gf.delay(gv.standardDelayAfterPageLoad);
                 await require('./mainfunctions/deploy.js')(page);
             } 
         }
         await gf.delay(gv.standardDelayAfterPageLoad);
-        await shutdown(page, browser);
+        await gf.shutdown(page, browser);
     } catch (error) {
         console.log (error);
     }
 };
 
-
-
-
-//  *********** CHILD FUNCTIONS ***********
-
-async function selectDeployablePackage(page) {
-    console.log('Trying to select a package to deploy...');
-    //wait untill the span element with innertext 'deploy' appears on the first row of the list of packages.
-    await page.waitForFunction("document.querySelectorAll('.gv_table')[0].getElementsByClassName('gv_row')[0].getElementsByClassName('gv_cell_Actions')[0].children[0].children[0].children[0].innerText == 'Deploy'", 
-                                {timeout: gv.longProcesTimeout});
-    await gf.delay(gv.standardDelayAfterFunction);
-    await page.evaluate(() => {
-        const deploybutton = document.querySelectorAll('.gv_table')[0].getElementsByClassName('gv_row')[0].getElementsByClassName('gv_cell_Actions')[0].children[0].children[0].children[0];
-        deploybutton.click();
-    });                    
-}
-
-async function shutdown(page, browser) {
-    console.log('Deploybot shutting down...');
-    await page.close();
-    await browser.close();
-}
 
 
 
