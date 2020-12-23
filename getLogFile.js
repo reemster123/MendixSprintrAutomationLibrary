d653ee18b9f04a0e5246a7f4f9ce6d9b49181601
reemster123 <42781824+reemster123@users.noreply.github.com>
2020-12-23 20:01:48 +0100

    Add files via upload
    created a function getlogfile which is a main function and can be ran from the commandline on its own

---
+++ getLogFile.js
@@ -0,0 +1,57 @@
+const gv = require('./globalvariables.json');
+const gf = require('./mainfunctions/genericfunctions.js');
+const puppeteer = require('puppeteer');
+const appName = gf.parseArgumentForIndex(2);
+const date = gf.parseArgumentForIndex(3);
+const downloadsFolder = require('downloads-folder');
+ 
+console.log(downloadsFolder());
+console.log('appName: '+appName);
+console.log('Date: '+date);
+
+main();
+
+async function main() {
+    try {
+        //open a browser and setup the page and viewport
+        console.log('Opening browser...');
+        const browser = await puppeteer.launch({headless: !gv.showProcesInBrowser});
+        const page = await require('./mainfunctions/setuppage.js')(browser);
+        
+        //login with credentials
+        await require('./mainfunctions/login.js')(page);
+        await gf.delay(gv.standardDelayAfterFunction);
+
+        // navigate from homepage to the environments page for the appName
+        const navigationSuccesfull = await require('./mainfunctions/navigateToApp.js')(appName, page);
+        if (navigationSuccesfull) {
+            console.log('We are now on the apps homepage...');
+            await gf.delay(gv.standardDelayAfterPageLoad);
+            // click on the "logs" menu item.
+            await require('./mainfunctions/clickOnAppMenuItem')('#formatstring_widget_formatstring_19' , page);
+            await gf.delay(gv.standardDelayAfterPageLoad);
+            
+            await gf.selectItemFromDropdown(page);
+            console.log('We are now on the logs page...');
+            await gf.delay(gv.standardDelayAfterPageLoad);
+
+            const selected = await require('./mainfunctions/selectListItem')(date, page);
+            await gf.delay(gv.standardDelayAfterFunction);
+            if (selected) {
+                const downloadButton = await gf.getElementForInnerText('download archived log', 'button', page);
+                await gf.delay(gv.standardDelayAfterFunction);
+                await downloadButton.click();
+                console.log('Clicked downloadbutton...');
+                await gf.delay(gv.standardDelayAfterPageLoad);
+                await require('./mainfunctions/displayLogFile.js')(date);
+            }
+            
+        }
+
+        await gf.delay(gv.standardDelayAfterPageLoad);
+        await gf.shutdown(page, browser);
+    } catch (err) {
+        console.log('Something went wrong: '+err);
+    }
+
+}
