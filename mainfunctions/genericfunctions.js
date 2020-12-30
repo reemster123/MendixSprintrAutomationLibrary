0029436d1ccc8032fedb145eeb933ee8b11d36b4
reemster123 <42781824+reemster123@users.noreply.github.com>
2020-12-30 14:57:52 +0100

    Update genericfunctions.js
    added function SelectFromRadioButtons,
    this is a generic function to use if you want to select  a radiobutton with a certain value.

--- mainfunctions/genericfunctions.js
+++ mainfunctions/genericfunctions.js
@@ -18,6 +18,7 @@ module.exports = {
         console.log('clicked next page...');
 
     },
+
     getElementForInnerText: async function(innertext, tagname, page) {
         const element = await page.evaluateHandle((name, tag) => {
             let elements = document.getElementsByTagName(tag);
@@ -29,6 +30,21 @@ module.exports = {
             return null;
         }, innertext, tagname );
         return element;
+
+    },
+
+    getElementForInnerTextAndParent: async function(innertext, tagname, parent, page) {
+        const element = await page.evaluateHandle((name, tag, parent) => {
+            let elements = parent.getElementsByTagName(tag);
+            elements = Array.from(elements);
+            el = elements.find(element => element.innerText.toLowerCase().includes(name));
+            if (typeof el !== 'undefined') {
+                return el;
+            }
+            return null;
+        }, innertext, tagname, parent );
+        return element;
+
     },
 
     getIdForInnerText: async function(innertext, tagname, page) {
@@ -41,7 +57,22 @@ module.exports = {
             } 
             return null;
         }, innertext, tagname );
-        return elementId;   
+        return elementId;
+        
+    },
+
+    getIdForInnerTextAndParent: async function(innertext, tagname, parent, page) {
+        const elementId = await page.evaluate((name, tag, parent) => { 
+            let elements = parent.getElementsByTagName(tag);
+            elements = Array.from(elements);
+            el = elements.find(element => element.innerText.toLowerCase().includes(name));
+            if (typeof el !== 'undefined') {
+                return el.id;
+            } 
+            return null;
+        }, innertext, tagname, parent );
+        return elementId;
+        
     },
 
     clickOkOnPopupDialog: async function(page) {
@@ -57,6 +88,7 @@ module.exports = {
             document.getElementsByClassName('modal-footer mx-dialog-footer')[0].getElementsByTagName('button')[0].click();
         });
         console.log('Clicked "Okay"...');
+
     },
 
     // select a row from a datagrid for the rowtitle and parentDataview
@@ -89,11 +121,11 @@ module.exports = {
         }, rowTitle, dvSelector);
     return rowSelected; 
     },
-    
+
     parseArgumentForIndex: function(i) {
         return process.argv[i].toLowerCase().split(gv.charToReplaceSpace).join(' ');
     },
-    
+
     selectItemFromDropdown: async function(page) {
         const dropdownButton = await page.waitForSelector('.dropdown-button');
         await dropdownButton.click();
@@ -107,16 +139,25 @@ module.exports = {
             console.info('clicked environment...');     
         }, dropdownMenu, gv.environment);
     },
-    
+
+    selectFromRadioButtons: async (surroundingElement, radiobuttonSelector, page) => {
+        const clickedDbOnly = await page.evaluate((element, selector) => {
+            const radioButton = element.querySelector(selector);
+            if (radioButton !== null) {
+                radioButton.click();
+                return true;
+            } 
+            return false;
+        }, surroundingElement, radiobuttonSelector); 
+        return clickedDbOnly;
+    },
+
     shutdown: async function(page, browser) {
         console.log('Deploybot shutting down...');
         await page.close();
         await browser.close();
     }
 
-    
     //  *** extra functions here... ***
 }
-
-
 
