eee0920ca916d409884304122a8ef76eae18103d
reemster123 <42781824+reemster123@users.noreply.github.com>
2021-01-18 21:13:11 +0100

    Update navigateToEnvironmentsPage.js
    clickOnAppMenuItem parameters changed so that had to be adjusted in this file (line 12)

--- mainfunctions/navigateToEnvironmentsPage.js
+++ mainfunctions/navigateToEnvironmentsPage.js
@@ -9,7 +9,7 @@ module.exports = async function(appName, page) {
     if (appfound) { 
         console.log('App with name "'+appName+'" exists...');
         // go to the environments page
-        await require('./clickOnAppMenuItem')('#formatstring_widget_formatstring_14', page);
+        await require('./clickOnAppMenuItem')('.mx-name-container4.submenu-group', 0, page);
         console.log('We are now on the environments page...');
     } else {
         console.log('Appname not found...');
